5dfbd822bea1666f15e4aaf8afefd9f0f48316b0
Abbychep <160326875+Abbychep@users.noreply.github.com>
2024-06-12 12:15:30 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,6 @@
+def user_input():
+    name= input("enter name:")
+    age= input("enter age")
+    location=input("enter location")
+    print("hello "+ name+" you are "+age+" years old "+" from "+location)
+user_input()
